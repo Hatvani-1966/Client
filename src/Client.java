0958e495b32ebc0e7a3349914b6a3802bde7e6a2
Hatvani-1966 <hatvaniattila@t-online.hu>
2020-10-30 20:26:02 +0100

    Firs version

---
+++ src/Client.java
@@ -0,0 +1,9 @@
+public class Client {
+
+    public static void main (String[]args)
+    Client Client - new Client();
+    client.name = "Jack London";
+    client.yearofBirth=1876;
+    System.out.print.ln(client.name);;
+    System.out.print.ln (client.yearofBirth);
+}
